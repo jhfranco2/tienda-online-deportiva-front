4b36a6f0474664c73cad10dca141d8152fcdf99c
Jhoan Mateo Franco Vargas <jfranco@sanjuandedios.org>
2023-06-08 15:12:59 -0500

    se crea la rama y el componente menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tiendaonlinedeportiva';
+  isMenuOpen: boolean = false;
+
+  toggleMenu(event: any) {
+    this.isMenuOpen = event.target.checked;
+  }
 }

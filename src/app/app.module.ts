e1842f226d0b34744a80120288afd5f1e45bc187
Jhoan Mateo Franco Vargas <jfranco@sanjuandedios.org>
2023-06-20 09:22:26 -0500

    se crean los componentes iniciales de contacto servicios footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,33 @@ import { MenuComponent } from './componentes/menu/menu.component';
 import {HttpClientModule} from '@angular/common/http';
 import { ItemsComponent } from './componentes/items/items.component';
 import { TiendaService } from './servicios/tienda.service';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { ServiciosComponent } from './componentes/servicios/servicios.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: ItemsComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'contacto', component: ContactoComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    ItemsComponent
+    ItemsComponent,
+    FooterComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [TiendaService],
   bootstrap: [AppComponent]

d22196c96ddf32c5e9484d2c382b7a76bedac8ee
Jhoan Mateo Franco Vargas <jfranco@sanjuandedios.org>
2023-06-21 16:14:42 -0500

    algunos estilos a crear y que redirija a la pagina principal

--- src/app/componentes/items/crear/crear.component.ts
+++ src/app/componentes/items/crear/crear.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Items } from 'src/app/modelos/tienda.model';
 import { TiendaService } from 'src/app/servicios/tienda.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crear',
@@ -10,24 +11,26 @@ import { TiendaService } from 'src/app/servicios/tienda.service';
 export class CrearComponent implements OnInit {
 
   item: Items = new Items();
-  imagenSeleccionada: File = new File([],'sin_imagen.png');
-    
-  constructor(private tiendaService: TiendaService) { }
+  imagenSeleccionada: File = new File([], 'sin_imagen.png');
+
+  constructor(private tiendaService: TiendaService, private router: Router) { }
 
   ngOnInit(): void {
+
   }
 
-  enviarItem(){
+  enviarItem() {
     this.tiendaService.crear(this.item).subscribe((
-      response)=>{
-        console.log(response);
-    }, (e:Error) =>{
+      response) => {
+      console.log(response);
+      this.router.navigate(['']);
+    }, (e: Error) => {
       console.log(this.item);
       console.log(e);
     });
   }
 
-  onFileSelected(event: any){
+  onFileSelected(event: any) {
     this.imagenSeleccionada = event.target.files[0];
     this.item.imagen = this.imagenSeleccionada;
 
